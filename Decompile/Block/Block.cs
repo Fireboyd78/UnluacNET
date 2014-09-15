74d65faeada916eb2819d7af7e94614cf2068866
Mark Ludwig <mk.ludwig1@gmail.com>
2014-09-14 22:10:48 -0700

    Some minor updates - trying to add as many classes as possible

--- Decompile/Block/Block.cs
+++ Decompile/Block/Block.cs
@@ -5,7 +5,88 @@ using System.Text;
 
 namespace UnluacNET
 {
-    public class Block
+    public class Block : Statement, IComparable<Block>
     {
+        protected readonly LFunction m_function;
+
+        public int Begin { get; set; }
+        public int End { get; set; }
+
+        public bool LoopRedirectAdjustment { get; set; }
+
+        public int ScopeEnd
+        {
+            get { return End - 1; }
+        }
+
+        public abstract bool Breakable { get; }
+        public abstract bool IsContainer { get; }
+        public abstract bool IsUnprotected { get; }
+        
+        public abstract int GetLoopback();
+
+        public abstract void AddStatement(Statement statement);
+
+        public int CompareTo(Block block)
+        {
+            if (Begin < block.Begin)
+            {
+                return -1;
+            }
+            else if (Begin == block.Begin)
+            {
+                if (End < block.End)
+                {
+                    return 1;
+                }
+                else if (End == block.End)
+                {
+                    if (IsContainer && !block.IsContainer)
+                    {
+                        return -1;
+                    }
+                    else if (IsContainer && block.IsContainer)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public bool Contains(Block block)
+        {
+            return (Begin <= block.Begin) && (End >= block.End);
+        }
+
+        public bool Contains(int line)
+        {
+            return (Begin <= line) && (line < End);
+        }
+
+        public Operation Process(Decompiler d)
+        {
+            // TODO: 'Block': Implement 'GenericOperation' of some sort, return statement
+            return null;
+        }
+
+        public Block(LFunction function, int begin, int end)
+        {
+            m_function = function;
+            
+            Begin = begin;
+            End = end;
+        }
     }
 }
