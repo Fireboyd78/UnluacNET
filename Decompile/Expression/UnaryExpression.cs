20713459698b0bbbe5673805999b75cc8a1cce09
Mark Ludwig <mk.ludwig1@gmail.com>
2014-09-20 18:31:26 -0700

    UnaryExpression class completed

--- Decompile/Expression/UnaryExpression.cs
+++ Decompile/Expression/UnaryExpression.cs
@@ -7,5 +7,34 @@ namespace UnluacNET
 {
     public class UnaryExpression : Expression
     {
+        private readonly string m_op;
+        private readonly Expression m_expression;
+
+        public override int ConstantIndex
+        {
+            get { return m_expression.ConstantIndex; }
+        }
+
+        public override void Print(Output output)
+        {
+            var precedence = (Precedence > m_expression.Precedence);
+            
+            output.Print(m_op);
+
+            if (precedence)
+                output.Print("(");
+
+            m_expression.Print(output);
+
+            if (precedence)
+                output.Print(")");
+        }
+
+        public UnaryExpression(string op, Expression expression, int precedence)
+            : base(precedence)
+        {
+            m_op = op;
+            m_expression = expression;
+        }
     }
 }
