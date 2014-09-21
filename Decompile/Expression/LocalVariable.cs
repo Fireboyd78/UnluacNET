f94089fe01740fb0f165b73ae6ca7837dfeec81a
Mark Ludwig <mk.ludwig1@gmail.com>
2014-09-20 18:16:29 -0700

    LocalVariable class completed

--- Decompile/Expression/LocalVariable.cs
+++ Decompile/Expression/LocalVariable.cs
@@ -5,8 +5,34 @@ using System.Text;
 
 namespace UnluacNET
 {
-    // TODO: Finish 'LocalVariable' class
     public class LocalVariable : Expression
     {
+        private readonly Declaration m_decl;
+
+        public override int ConstantIndex
+        {
+            get { return -1; }
+        }
+
+        public override bool IsBrief
+        {
+            get { return true; }
+        }
+
+        public override bool IsDotChain
+        {
+            get { return true; }
+        }
+
+        public override void Print(Output output)
+        {
+            output.Print(m_decl.Name);
+        }
+
+        public LocalVariable(Declaration decl)
+            : base(PRECEDENCE_ATOMIC)
+        {
+            m_decl = decl;
+        }
     }
 }
